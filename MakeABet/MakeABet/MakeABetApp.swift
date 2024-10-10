af4e81b41cf01eb28748cb8dadf1c2a8f2f846a5
ogalexan <123039321+ogalexan@users.noreply.github.com>
2024-10-10 11:19:23 -0400

    Added Project

---
+++ MakeABet/MakeABet/MakeABetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeABetApp.swift
+//  MakeABet
+//
+//  Created by Olivia Alexander on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MakeABetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MakeABet/MakeABetTests/MakeABetTests.swift
@@ -0,0 +1,17 @@
+//
+//  MakeABetTests.swift
+//  MakeABetTests
+//
+//  Created by Olivia Alexander on 10/10/24.
+//
+
+import Testing
+@testable import MakeABet
+
+struct MakeABetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
