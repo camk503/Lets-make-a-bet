03bbe8853ae438a82b24ed441a3f1e0d2ec7f43f
hannah <hgsherid@uvm.edu>
2024-10-18 15:02:28 -0400

    Implemented search of top 1000 artists

--- MakeABet/MakeABet/SearchView.swift
+++ MakeABet/MakeABet/SearchView.swift
@@ -5,7 +5,7 @@
 //  Created by Hannah Sheridan on 10/17/24.
 //  HELPFUL DOCS:
 //      Search: https://developer.apple.com/documentation/swiftui/adding-a-search-interface-to-your-app
-//
+//              https://www.appcoda.com/swiftui-searchable/
 
 import SwiftUI
 
@@ -22,36 +22,53 @@ struct SearchView : View {
     @State var connect : LastAPI = LastAPI()
     @State var isLoading : Bool = true
     @State var artistList : [Artist] = []
+    // Search result
+    @State var results : [Artist] = []
     
     var body: some View {
-        VStack {
-            HStack {
-                Button("Search Artists") {
-                    page = 0
-                }.buttonStyle(.borderedProminent)
-                Button("Search Profiles") {
-                    page = 1
-                }.buttonStyle(.borderedProminent)
-            }
+        NavigationView {
             VStack {
-                if page == 0 {
-                    // Print current top artists
-                    List(artistList.indices, id: \.self) { index in
-                        
-                        let artist = artistList[index]
-                        
-                        ArtistSearchView(artist: artist)
-                    }
+                HStack {
+                    Button("Search Artists") {
+                        page = 0
+                    }.buttonStyle(.borderedProminent)
+                    Button("Search Profiles") {
+                        page = 1
+                    }.buttonStyle(.borderedProminent)
                 }
-                else if page == 1 {
-                    Text("Put profiles here for search")
+                VStack {
+                    if page == 0 {
+                        // Print current top artists
+                        List(results.indices, id: \.self) { index in
+                            
+                            let artist = results[index]
+                            
+                            ArtistSearchView(artist: artist)
+                        }
+                        
+                    }
+                    else if page == 1 {
+                        Text("Put profiles here for search")
+                    }
+                    
                 }
                 
+            }
+        }
+        .searchable(text: $searchText)
+        .onChange(of: searchText) { searchText in
+            if !searchText.isEmpty {
+                // TODO: Right now this can only search top 1000 artists
+                    // Should be able to search ALL artists
+                results = artistList.filter { $0.name.lowercased().contains(searchText.lowercased())}
+            }
+            else {
+                results = artistList
             }
             
         }
         .onAppear() {
-            connect.fetchTopArtists(limit: 50) { result in
+            connect.fetchTopArtists(limit: 1000) { result in
                 switch result {
                     
                 case .success(let fetchedArtists):
@@ -62,6 +79,7 @@ struct SearchView : View {
                     }
                     
                     self.artistList = fetchedArtists
+                    self.results = fetchedArtists
                     
                 case .failure(let error):
                     self.isLoading = false
