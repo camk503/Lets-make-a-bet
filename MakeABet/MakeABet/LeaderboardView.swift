e08389e91ce1a3378b05f9c0fc6bec83b67c5add
ogalexan <123039321+ogalexan@users.noreply.github.com>
2024-11-23 15:49:09 -0500

    Add files via upload

--- MakeABet/MakeABet/LeaderboardView.swift
+++ MakeABet/MakeABet/LeaderboardView.swift
@@ -13,6 +13,10 @@ struct LeaderboardView : View {
     
     let db = Firestore.firestore()
     
+    private let profileModel = ProfileModel()
+    private let leaderboardModel = LeaderboardModel()
+
+    
     var body : some View {
         NavigationView() {
             ZStack {
@@ -31,6 +35,20 @@ struct LeaderboardView : View {
             }.navigationTitle("Leaderboard")
         }
     }
+    
+    private func fetchScore() {
+            profileModel.getLineup { result in
+                DispatchQueue.main.async {
+                    self.isLoading = false
+                    switch result {
+                    case .success(let score):
+                        self.lineup = score
+                    case .failure(let error):
+                        print("Error loading lineup: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
 }
 #Preview() {
     LeaderboardView()
